203454e9196768497ca9e3f179c5a64370b2efea
Tejas Nanoti <tejasnanoti2@gmail.com>
2024-02-16 23:44:55 +0530

    style[app/page | components/Navbar]:💅 Enhanced Landing page & Navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import { Avatar ,AvatarFallback } from "@/components/ui/avatar";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
@@ -5,26 +7,35 @@ import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle }
 import { Separator } from "@/components/ui/separator";
 import Tooltip from "@/components/Tooltip";
 import { Input } from "@/components/ui/input";
-import { SendHorizonal } from "lucide-react";
+import { Heart, MessageCircle, Repeat, SendHorizonal } from "lucide-react";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+
+  const [likes, setLikes] = useState(42)
+  const router = useRouter()
+
+  const handleLike = () => {
+    if (likes == 42) setLikes(43)
+    else setLikes(42)
+  }
+
   return (
     <div>
-      <div id="header" className="flex flex-col items-center mt-40">
-        <h1 className="text-6xl">Zennotes</h1>
-        <p className="text-center">Your ultimate companion to practice mindfulness. <br />Connect. Practice. Enlighten.</p>
+      <div id="header" className="flex flex-col items-center mt-40 md:mt-32 md:flex-row md:justify-around">
+        <div className="text-center md:text-left">
+          <h1 className="text-6xl">Zennotes</h1>
+          <p>Your ultimate companion to practice mindfulness. <br />Connect. Practice. Enlighten.</p>
+        </div>
 
-        <Card className="my-8 w-8/12">
+        <Card className="my-8 w-4/5 sm:w-2/3 md:w-1/3">
           <CardHeader>
             <div className="flex flex-row items-center">
               <Avatar>
                 <AvatarFallback>JD</AvatarFallback>
               </Avatar>
-              <CardTitle className="mx-2">John Doe</CardTitle>
-              {/* <div className="mx-2">
-                <CardTitle>John Doe</CardTitle>
-                <CardDescription>@meditating_john</CardDescription>
-              </div> */}
+              <CardTitle className="mx-2 hover:underline hover:cursor-pointer">John Doe</CardTitle>
             </div>
           </CardHeader>
           <Separator />
@@ -32,8 +43,8 @@ export default function Home() {
           <CardContent className="p-4">
             <p>Believe nothing, no matter where you read it, or who said it, no matter if I have said it, unless it agrees with your own reason and your own common sense. <br /> - Buddha</p>
             <div className="my-2">
-              <Badge variant='outline'>buddha</Badge>
-              <Badge variant='outline'>meditation</Badge>
+              <Badge variant='outline' className="mx-1">buddha</Badge>
+              <Badge variant='outline' className="mx-1">meditation</Badge>
             </div>
             <Tooltip label="self-growth">
               <Button variant="badge">
@@ -43,8 +54,29 @@ export default function Home() {
           </CardContent>
           <Separator />
 
+          <div className="flex flex-row justify-evenly items-center py-2">
+            <Tooltip label="Heart">
+              <Button variant='ghost' className="flex flex-row items-center p-2" onClick={handleLike}>
+                <Heart size={18} className={`mx-1 ${likes == 43 && 'text-red-700'}`} /> 
+                {likes}
+              </Button>
+            </Tooltip>
+            <Tooltip label="Quote">
+              <Button variant='ghost' className="flex flex-row items-center p-2">
+                <MessageCircle size={18} className="mx-1" /> 
+                20
+              </Button>
+            </Tooltip>
+            <Tooltip label="Repost">
+              <Button variant='ghost' className="flex flex-row items-center p-2">
+                <Repeat size={18} className="mx-1" /> 
+                14
+              </Button>
+            </Tooltip>
+          </div>
+
           <div className="flex">
-            <Input  className="rounded-tl-none rounded-r-none" placeholder="Reply..." />
+            <Input  className="rounded-tl-none rounded-r-none" placeholder="Reply..."/>
             <Button className="rounded-tr-none rounded-l-none"><SendHorizonal size={18} className="m-2" /></Button>
           </div>
         </Card>
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,12 +1,19 @@
+"use client"
+
 import { LogIn, Menu, User } from "lucide-react"
 import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from "./ui/sheet"
 import { Button } from "./ui/button"
 import { ModeToggle } from "./mode-toggle"
 import { NavigationMenu } from "./ui/navigation-menu"
+import { useRouter } from "next/navigation"
 
 const Navbar = () => {
+
+    const router = useRouter()
+
   return (
-    <nav id="navbar" className="p-4 px-8 flex flex-row justify-between items-center">
+    // <nav id="navbar" className="p-4 px-8 flex flex-row justify-between items-center">
+    <nav className="p-4 py-6 sm:p-3 sm:mt-0 sm:text-left flex justify-between items-center z-10 fixed w-full dark:bg-[#0a0a0a] dark:bg-opacity-70 backdrop-blur-sm top-0">
         <h3>Zennotes</h3>
 
         {/* MOBILE NAVIGATION */}
@@ -16,8 +23,8 @@ const Navbar = () => {
             </SheetTrigger>
             <SheetContent>
                 <div className="my-5 flex flex-col justify-center">
-                    <Button variant='outline' className="m-2 w-full">Log In <LogIn size={18} className="mx-1" /></Button>
-                    <Button variant='outline' className="m-2 w-full">Sign Up <User size={18} className="mx-1" /></Button>
+                    <Button variant='outline' className="m-2 w-full" onClick={() => router.push('/login')}>Log In <LogIn size={18} className="mx-1" /></Button>
+                    <Button variant='outline' className="m-2 w-full" onClick={() => router.push('/signup')}>Sign Up <User size={18} className="mx-1" /></Button>
                     <ModeToggle className="w-full" />
                 </div>
             </SheetContent>
